2e56b0d783251ea761e71d68c4e5d1865a0059a4
bigfa <bigfa@LAPTOP-SS9VD3K4>
2023-11-14 01:17:03 -0500

    Add project files.

---
+++ Library Management System/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace Library_Management_System.Models
+{
+    public class Book
+    {
+        public Book()
+        {
+            // Initialize non-nullable properties in the constructor
+            Author = ""; // You can use an empty string or another default value
+        }
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+}
